19e4b020aefe8a0e86005e36d540726b4a6bac9e
zonika <zchodosh@conncoll.edu>
2015-10-14 10:28:57 -0400

    stacks on stacks on stacks

--- find_closing_parenthesis.rb
+++ find_closing_parenthesis.rb
@@ -1,3 +1,16 @@
 def find_closing_parenthesis(sentence, open_position)
-
+  stack = []
+  ret = 0
+  sentence.split('').each_with_index do |chr, i|
+    if chr == '('
+      stack.push(i)
+    elsif chr == ')'
+      n = stack.pop
+      if n == open_position
+        ret = i
+        break
+      end
+    end
+  end
+  ret
 end
